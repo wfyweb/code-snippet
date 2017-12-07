2340772c82d98d12c79eef7e90f76769f36da2d6
阿怪 <862704559@qq.com>
2017-12-07 11:11:09 +0800

    add note

--- start-js/12-7/index.js
+++ start-js/12-7/index.js
@@ -1,5 +1,15 @@
+//obj = {id:1,name:'gg',...} => arr = [{id:1},{name:'gg'},{...}...]
+//对象转化为数组
+//1.建立一个空数组，
+//2 for in 遍历对象
+//3 push到数组里面
+// * * * * * * * * * * *  
+// *    key 是表达式    * 
+// *    obj[key]是值    *  
+// * * * * * * * * * * * 
+
 var objPeople = {id:0,name:'prerson',mood:'quiet',target:'bent'};
 var arrlist = [];
 for(key in objPeople){
-    console.log(key)
+    console.log(key,objPeople[key])
 }
