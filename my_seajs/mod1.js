78c0ffa7dbbaa594bdc4bce7833eb5c77b8b1c26
阿怪 <862704559@qq.com>
2018-01-14 22:31:15 +0800

    add my_seajs

---
+++ my_seajs/mod1.js
@@ -0,0 +1,10 @@
+
+"use strict";
+define(function(require,exports,module){
+    exports.a = 13;
+    exports.b = 22;
+});
+
+
+// 不添加严格模式会失败
+
---
+++ my_seajs/my_sea.js
@@ -0,0 +1,22 @@
+const seajs = {
+    use(path,fn_end){
+        $.ajax({
+            url:path,
+            success(str){
+               function define(fn){
+                    let module = {
+                        exports:{}
+                    };
+
+                    fn(function(){},module.exports,module);
+                    
+                    fn_end(module.exports);
+               }
+               eval(str);
+            },
+            error(){
+                alert('失败');
+            },
+        })
+    }
+}
