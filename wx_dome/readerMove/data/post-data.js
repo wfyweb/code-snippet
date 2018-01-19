f4a15a83e6dd940d22d8183cdafb354816ab26f0
阿怪 <862704559@qq.com>
2018-01-19 17:35:15 +0800

    文章详情页-静态

---
+++ wx_dome/readerMove/data/post-data.js
@@ -0,0 +1,39 @@
+var local_database = [
+  {
+    date: 'sep 18 2016',
+    title: '正是虾肥蟹壮时',
+    imgSrc: '/images/post/crab.png',
+    avatar: '/images/avatar/1.png',
+    content: '本文档将带你一步步创建完成一个微信小程序，并可以在手机上体验该小程序的实际效果。这个小程序的首页将会显示欢迎语以及当前用户的微信头像，点击头像',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/post/crab.png',
+    author: '李白'
+  },
+  {
+    date: 'sep 18 2016',
+    title: '正是虾肥蟹壮时',
+    imgSrc: '/images/post/crab.png',
+    avatar: '/images/avatar/1.png',
+    content: '本文档将带你一步步创建完成一个微信小程序，并可以在手机上体验该小程序的实际效果。这个小程序的首页将会显示欢迎语以及当前用户的微信头像，点击头像',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/post/crab.png',
+    author: '李白'
+  },
+  {
+    date: 'sep 18 2016',
+    title: '正是虾肥蟹壮时',
+    imgSrc: '/images/post/crab.png',
+    avatar: '/images/avatar/1.png',
+    content: '本文档将带你一步步创建完成一个微信小程序，并可以在手机上体验该小程序的实际效果。这个小程序的首页将会显示欢迎语以及当前用户的微信头像，点击头像',
+    reading: '112',
+    collection: '96',
+    headImgSrc: '/images/post/crab.png',
+    author: '李白'
+  }
+]
+
+module.exports={
+  postList: local_database
+}
