8b1d62964a63ff154489efbd4e9e429ddfda7109
阿怪 <862704559@qq.com>
2018-01-23 16:53:20 +0800

    异步与同步

--- wx_dome/readerMove/pages/posts/posts-detail/posts-detail.js
+++ wx_dome/readerMove/pages/posts/posts-detail/posts-detail.js
@@ -1,70 +1,135 @@
 var postData = require("../../../data/post-data.js");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //新闻列表传递的索引值
+    //文章索引
     var id = options.id;
-    // 索引映射对应的数据
-    //this.data
+
     this.setData({
-      postData: postData.postList[id],
-      currentPostId: id
-    });
-    console.log(this.data.currentPostId)
-    //wx.setStorageSync("name","暴风")
-    // var postCollection = {
-    //   1:true,
-    //   2:false,
-    //   3:true
-    // };
+      currentId: id,
+      postData:postData.postList[id]
+    })
+    // var post_collection={
+    //     1:true,
+    //     2:false,
+    //     3:false
+    // }
     var postsCollection = wx.getStorageSync("post-collection");
-    if (postsCollection) {
+    if (postsCollection){
       var postCollection = postsCollection[id];
       this.setData({
-        collection: postCollection
+        collection:postCollection
       })
-    } else {
+    }else{
       var postsCollection = {};
-      var postCollection = false;
+      postsCollection[id] = false;
       wx.setStorageSync("post-collection", postsCollection);
     }
+
   },
-  onCollectionTap: function (event) {
+  // 点击收藏
+  onCollectionTop:function(){
+    // 同步提示框
+    //this.getPostsCollectedSyc();
+    //异步提示框
+    this.getPostsCollectedAyc()
+
+   
+  },
+  //异步提示框 （不推荐）
+  getPostsCollectedAyc: function(){
+    var that = this;
+    // 更新缓存值 
+    wx.getStorage({
+      key: 'post-collection',
+      success:function(res){
+        var postsCollection =  res.data;
+        var postCollection = postsCollection[that.data.currentId];
+        // 取反
+        postCollection = !postCollection;
+        // 响应key值
+        postsCollection[that.data.currentId] = postCollection;
+        that.setShowToastSync(postsCollection, postCollection);
+      }
+    });
+    
+  },
+  // 同步提示框 （推荐）
+  getPostsCollectedSyc:function(){
     var postsCollection = wx.getStorageSync("post-collection");
-    var postCollection = postsCollection[this.data.currentPostId];
+    var postCollection = postsCollection[this.data.currentId];
     postCollection = !postCollection;
-    postsCollection[this.data.currentPostId] = postCollection;
+    // 响应key值
+    postsCollection[this.data.currentId] = postCollection;
+    this.setShowToastSync(postsCollection, postCollection);
+  }, 
+  setShowToastSync: function (postsCollection, postCollection){
     wx.setStorageSync("post-collection", postsCollection);
     this.setData({
       collection: postCollection
     })
-    // wx.showToast({
-    //   title: postCollection ? '收藏成功' : '取消成功',
-    //   icon: 'success',
-    //   duration: 1000
-    // })
+    // 提示框
+    wx.showToast({
+      title: postCollection ? '收藏成功' : "取消成功",
+      icon: 'success',
+      duration: 1000
+    })
+  },
+
+///////////////////////////////////////////////////////////////////////////
+
+  
+  // 提示选项框 （不推荐）
+  showModalSync: function (postsCollection, postCollection) {
+    var that = this;
     wx.showModal({
-      title: '提示',
-      content: '这是一个模态弹窗',
+      title: '收藏',
+      content: postCollection ? '取消收藏？' : '确定收藏？',
       success: function (res) {
         if (res.confirm) {
           console.log('用户点击确定')
-        } else if (res.cancel) {
-          console.log('用户点击取消')
+          // 取反
+          postCollection = !postCollection;
+          // 响应key值
+          postsCollection[that.data.currentId] = postCollection
+          // 更新缓存值
+          wx.setStorageSync("post-collection", postsCollection);
+          //映射
+          that.setData({
+            collection: postCollection
+          })
         }
       }
     })
-  }
+  },
 
+  // ​分享菜单
+  showActionSheet: function () {
+    var itemList = ['分享到微信好友', '分享到朋友圈', '分享到微博'];
+    // 显示操作菜单列表
+    wx.showActionSheet({
+      itemList: itemList,
+      success: function (res) {
+        // 显示提示框
+        wx.showModal({
+          title: "用户" + itemList[res.tapIndex],
+          content: "用户是否取消？" + res.cancel + "现在无法实现分享功能，什么时候能支持呢"
+        })
+      }
 
+    })
+  },
+
+  
 })
