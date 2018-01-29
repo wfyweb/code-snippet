4b40f269eefdcbf718231ed042f0ccdffef7add4
阿怪 <862704559@qq.com>
2018-01-29 18:12:12 +0800

    template 嵌套

--- wx_dome/readerMove/pages/welcome/welcome.js
+++ wx_dome/readerMove/pages/welcome/welcome.js
@@ -1,15 +1,21 @@
 // pages/welcome/welcome.js
 Page({
   onTap:function(){
-    //跳转子页面
-    wx.navigateTo({
-      url: '/pages/posts/posts',
-    })
+    // redirectTo和navigateTo不能再跳转到带有tab选项卡的页面
+    //跳转子页面 
+    // wx.navigateTo({
+    //   url: '/pages/posts/posts',
+    // })
 
     // 跳转平行页面
     // wx.redirectTo({
     //   url: '/pages/posts/posts',
     // })
-  },
+   
+     wx.switchTab({
+        url: "/pages/posts/posts"
+    })
+   
+  }
   
 })
