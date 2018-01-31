8da18403a77f8c5a02546d66a527d65abeb606be
阿怪 <862704559@qq.com>
2018-01-31 17:30:39 +0800

    引入豆瓣接口

--- wx_dome/readerMove/pages/welcome/welcome.js
+++ wx_dome/readerMove/pages/welcome/welcome.js
@@ -1,21 +1,12 @@
-// pages/welcome/welcome.js
 Page({
-  onTap:function(){
-    // redirectTo和navigateTo不能再跳转到带有tab选项卡的页面
-    //跳转子页面 
-    // wx.navigateTo({
-    //   url: '/pages/posts/posts',
-    // })
-
-    // 跳转平行页面
-    // wx.redirectTo({
-    //   url: '/pages/posts/posts',
-    // })
-   
-     wx.switchTab({
-        url: "/pages/posts/posts"
-    })
-   
-  }
-  
+    onTap: function (event) {
+        // wx.navigateTo({
+        //     url:"../posts/post"
+        // });
+        
+        wx.switchTab({
+            url: "../posts/post"
+        });
+        
+    }
 })
